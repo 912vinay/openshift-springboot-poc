1cdf95869a580e19f166c330b214bdb842cd6b36
912vinay <912vinay@gmail.com>
2021-10-30 13:22:05 +0530

    open shift trial

---
+++ src/test/java/com/learn/openshiftspringpoc/OpenshiftSpringPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.openshiftspringpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenshiftSpringPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
